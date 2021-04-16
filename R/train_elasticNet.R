944af66c83a3337637f47931fda10db27e691b57
spocks <arvindmer@gmail.com>
2021-04-15 21:41:02 -0400

    updated lambda and alpha values

--- R/train_elasticNet.R
+++ R/train_elasticNet.R
@@ -1,18 +1,22 @@
 library(caret)
-d5 <- readRDS("../data/Bortezomib_500.rds")
+#d5 <- readRDS("../data/Bortezomib_500.rds")
 #d1 <- readRDS("../data/Bortezomib_100.rds")
+
+d5 <- readRDS("~/Downloads/Cediranib_500.rds")
 
 x = d5$X; y=d5$Y$aac
 
 method = "glmnet"
-tgrid <- expand.grid(alpha=seq(0, 1, 0.2), lambda=seq(0, 10, 1))
-tcontrol <- trainControl(method="repeatedcv",
-                         number= 4,
-                         repeats = 4,
-                         search="grid",
-                         savePredictions ="all",
-                         allowParallel = TRUE,
-                         verboseIter=FALSE)
+tgrid <- expand.grid(alpha=seq(0.0, 1, 0.1),
+                     lambda = 10^seq(-3, 3, length = 10))
+
+tcontrol <- trainControl(method = "cv", #"repeatedcv",
+                         number= 4, #repeats = 4,
+                         search="grid", allowParallel = TRUE,
+                         verboseIter=FALSE,
+                         savePredictions=c("all", "final", "none")[3],
+                         returnResamp   =c("all", "final", "none")[3],
+                         returnData = FALSE)
 
 trainIndex <- createDataPartition(y, p = .8, list = TRUE, times = 5)
 mod100List <- list(); mod500List <- list();
@@ -38,11 +42,14 @@ for(res in names(trainIndex))
   ts500 <- testTransformed[ , var500]
 
   ##-----------
-  getTrainMod <- function(trData, y, trIndx, tgrid, tcontrol, tsIndx, testData,res)
+  train_elastic_net_model <- function(trData, y, trIndx, tgrid, tcontrol, tsIndx, testData,res)
   {
-    model <- train(x=trData, y=y[trIndx],method="glmnet", tuneGrid=tgrid,
-                      trControl=tcontrol)
-    model$trainingData <- NULL
+    model <- train(x=trData, y=y[trIndx],method="glmnet", trControl=tcontrol,
+                   tuneGrid=tgrid #tuneLength = 10 #
+                   )
+    #model$trainingData <- NULL ##no need as returnData=F
+    model$finalModel$call <- NULL
+    ##----feature names are in model$finalModel$xNames ------
 
     predx <- data.frame(index = tsIndx, pred=predict(model, testData),
                         obs=y[tsIndx], resample=res)
@@ -52,17 +59,17 @@ for(res in names(trainIndex))
     return(list(model=model, pred=predx, performance=performance))
   }
 
-  model100 <- getTrainMod(tr100, y, trIndx,tgrid, tcontrol, tsIndx, ts100, res)
-  model500 <- getTrainMod(tr500, y, trIndx,tgrid, tcontrol, tsIndx, ts500, res)
+  model100 <- train_elastic_net_model(tr100, y, trIndx,tgrid, tcontrol, tsIndx, ts100, res)
+  model500 <- train_elastic_net_model(tr500, y, trIndx,tgrid, tcontrol, tsIndx, ts500, res)
 
   mod100List[[res]] <- model100
   mod500List[[res]] <- model500
 }
 
 mean(sapply(mod100List, function(i)i$performance['cor']))
-## [1] 0.3469093
+## [1] 0.4272153
 
 mean(sapply(mod500List, function(i)i$performance['cor']))
-## [1] 0.3530966
+## [1] 0.4641324
 
 
