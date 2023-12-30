570854a428924c9ca1157be21d0aab8e0e4a1f24
ArvinZaker <arv.zaker@gmail.com>
2023-12-30 10:56:34 -0500

    added a new script for the new figure

--- code/Fig-2_ML_results.R
+++ code/Fig-2_ML_results.R
@@ -1,3 +1,6 @@
+# NOTE: this figure was modified from the old script to include the new
+# results (REF, MRMR, and GA)
+library(readxl)
 library(ggplot2)
 library(ggpubr)
 # library(gridExtra)
@@ -6,14 +9,14 @@ library(ggpubr)
 # library(reshape2); ; library(ggrepel)
 
 # source("https://gist.githubusercontent.com/benmarwick/2a1bb0133ff568cbe28d/raw/fb53bd97121f7f9ce947837ef1a4c65a73bffb3f/geom_flat_violin.R")
-source("R/geom_flat_violin.R")
+source("./code/geom_flat_violin.R")
 
 raincloud_theme <- theme(
   text = element_text(size = 10),
   axis.title.x = element_text(size = 16),
   axis.title.y = element_text(size = 16),
   axis.text = element_text(size = 14),
-  axis.text.x = element_text(angle = 45, vjust = 0.5),
+  axis.text.x = element_text(angle = 45, vjust = 0.5, color = "black"),
   legend.title = element_text(size = 16),
   legend.text = element_text(size = 16),
   legend.position = "right",
@@ -27,10 +30,31 @@ raincloud_theme <- theme(
 
 ## -------------------------------------------------------------
 
-create_plot <- function(df, title = NA) {
-  fetMeth <- colnames(df)[colnames(df) != "drug"]
+create_plot <- function(dft, title = NA) {
+  # fetMeth <- colnames(df)[colnames(df) != "drug"]
+  dft <- as.data.frame(dft)
+  #   fetMeth <- c("var-100", "var-500", "L1000-tm", "L1000", "cor-500", "text-mining")
+  fetMeth <- c(
+    "var-500", "var-100", "L1000", "L1000-tm", "cor-500",
+    "RFE", "MRMR", "GA",
+    "text-mining"
+  )
 
-  cl <- c("#B09C85FF", "#91D1C2FF", "#4DBBD5FF", "#3C5488FF", "#00A087FF", "#DC0000FF")
+  dft$Type <- factor(dft$Type, levels = fetMeth)
+
+  # cl <- c("#B09C85FF", "#91D1C2FF", "#4DBBD5FF", "#3C5488FF", "#00A087FF", "#DC0000FF")
+  # cl <- c(
+  #   "#B09C85FF", "#91D1C2FF", "#4DBBD5FF", "#3C5488FF", "#00A087FF",
+  #   "#DC0000FF", "#173dd3", "#29df49", "#24dd9d"
+  # )
+  # cl <- c(
+  #   "#B09C85FF", "#91D1C2FF", "#4DBBD5FF", "#3C5488FF", "#00A087FF",
+  #   "#DC0000FF", "#85469d", "#bf4aa5", "#446e35"
+  # )
+  cl <- c(
+    "#B09C85FF", "#91D1C2FF", "#4DBBD5FF", "#3C5488FF", "#00A087FF",
+    "#85469d", "#bf4aa5", "#446e35", "#DC0000FF"
+  )
   names(cl) <- fetMeth
 
   my_comparisons <- lapply(
@@ -38,9 +62,11 @@ create_plot <- function(df, title = NA) {
     function(i) c(i, fetMeth[length(fetMeth)])
   )
 
-  df$drug <- rownames(df)
+  # df$drug <- rownames(df)
 
-  dft <- reshape2::melt(df, id.vars = "drug", variable.name = "Type")
+  # dft <- reshape2::melt(df, id.vars = "drug", variable.name = "Type")
+  # print(head(dft))
+  # stop()
 
   g <- ggplot(data = dft, aes(y = value, x = Type, fill = Type)) +
     geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .95) +
@@ -62,53 +88,56 @@ create_plot <- function(df, title = NA) {
 
   g <- g + scale_color_manual(values = cl) + scale_fill_manual(values = cl)
   g <- g + stat_compare_means(
-    comparisons = my_comparisons, method = "t.test", paired = TRUE,
+    comparisons = my_comparisons, method = "wilcox.test", paired = TRUE,
     # label.y = 0.7,
     size = 2.5
   )
-  g <- g + scale_y_continuous(breaks = seq(0, 1, 0.1))
+  g <- g + scale_y_continuous(breaks = seq(-0.4, 1, 0.2))
   g + theme(
     axis.text.x = element_text(
       face = "bold", # color="#993333",
+      color = "black",
       size = 8, angle = -0
     ),
     axis.text.y = element_text(
       face = "bold", # color="#993333",
+      color = "black",
       size = 9, angle = 0
     ),
     axis.title.y = element_text(face = "bold", size = 10)
   )
+  return(g)
 }
 
 ## --------------------------------------------------------------
 ## ---- Random-forest training and test plot --------------------
-trRF <- readRDS("result/train_test/train_RandomForest_Reg.rds")
-write.csv(trRF, "result/train_test/train_RandomForest_Reg.csv")
+
+trRF <- read_xlsx("./result/mlModelMetrics.xlsx", sheet = "rf_train")
+trRF <- trRF[, c("drug", "geneFilterMethod", "pearsonCor")]
+colnames(trRF) <- c("drug", "Type", "value")
 pltTRRF <- create_plot(trRF)
-print(pltTRRF + ggtitle("Random Forest training"))
-
-tsRF <- readRDS("result/train_test/test_RandomForest_Reg.rds")
-write.csv(tsRF, "result/train_test/test_RandomForest_Reg.csv")
+## ------------------------------------------------------------
+tsRF <- read_xlsx("./result/mlModelMetrics.xlsx", sheet = "rf_test")
+tsRF <- tsRF[, c("drug", "geneFilterMethod", "pearsonCor")]
+colnames(tsRF) <- c("drug", "Type", "value")
 pltTSRF <- create_plot(tsRF)
-print(pltTSRF + ggtitle("Random Forest test"))
-
 ## ------------------------------------------------------------
 ## ---- Elastic-Net training and test plot --------------------
-trEN <- readRDS("result/train_test/train_ElasticNet_Reg.rds")
-write.csv(trEN, "result/train_test/train_ElasticNet_Reg.csv")
+trEN <- read_xlsx("./result/mlModelMetrics.xlsx", sheet = "glmnet_train")
+trEN <- trEN[, c("drug", "geneFilterMethod", "pearsonCor")]
+colnames(trEN) <- c("drug", "Type", "value")
 pltTREN <- create_plot(trEN)
-print(pltTREN + ggtitle("Elastic-Net training"))
+## ------------------------------------------------------------
 
-
-tsEN <- readRDS("result/train_test/test_ElasticNet_Reg.rds")
-write.csv(tsEN, "result/train_test/test_ElasticNet_Reg.csv")
+tsEN <- read_xlsx("./result/mlModelMetrics.xlsx", sheet = "glmnet_test")
+tsEN <- tsEN[, c("drug", "geneFilterMethod", "pearsonCor")]
+colnames(tsEN) <- c("drug", "Type", "value")
 pltTSEN <- create_plot(tsEN)
-print(pltTSEN + ggtitle("Elastic-Net test"))
-
 ## ------------------------
-pdf("result/Fig-2_ML_results.pdf", width = 8.3, height = 8.0)
+pdf("result/az_Fig-2_ML_results.pdf", width = 8.3, height = 8.0)
 print(ggarrange(pltTRRF, pltTSRF, pltTREN, pltTSEN,
-  # labels = c("A", "B", "C"),
+  labels = c("A", "B", "C", "D"),
   ncol = 2, nrow = 2
 ))
 dev.off()
+print("done")
