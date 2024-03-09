90922a1c8e89a691d83dc521debc30b4815975e9
ArvinZaker <arv.zaker@gmail.com>
2024-03-09 18:43:39 -0500

    updated bar plot

--- code/X_bar_plot.R
+++ code/X_bar_plot.R
@@ -4,8 +4,8 @@ library(ggpubr)
 library(ggsci)
 fPath <- "./mlModelMetrics_pathway.xlsx"
 fTxt <- "./result/Supplementary-data-2-performance-indexes.xlsx"
-pdf("./ML_results_path.pdf", height = 3, width = 5)
 sheets <- c("ElasticNet perf metrics-test", "RandomForest perf metrics-test")
+p <- list()
 for (i in sheets) {
   path <- read_xlsx(fPath, sheet = i)[, c("drug", "pearsonCor", "feature selection method")]
   txt <- read_xlsx(fTxt, sheet = i)
@@ -15,18 +15,19 @@ for (i in sheets) {
   ]
   d <- rbind(path, txt)
   d$`feature selection method` <- factor(d$`feature selection method`, levels = c("text-mining", "pathway"))
-  p <- ggplot(d, aes(x = drug, y = pearsonCor, fill = `feature selection method`)) +
+  p[[i]] <- ggplot(d, aes(x = drug, y = pearsonCor, fill = `feature selection method`)) +
     geom_bar(position = "dodge", stat = "identity", width = 0.75) +
     scale_fill_npg(
       labels = c("Text-mining", "Pathway"),
-      name = "Feature selection\nmethod"
+      name = "Feature selection method"
     ) +
-    ggtitle(i) +
+    ggtitle(gsub("perf metrics-test", "", i)) +
     geom_hline(yintercept = 0, color = "black", linewidth = 0.5) +
     xlab("Drug") +
     ylab("Pearson correlation") +
     scale_y_continuous(expand = c(0, 0)) +
     theme_classic()
-  plot(p)
 }
+pdf("./ML_results_path.pdf", height = 3, width = 6)
+plot(ggarrange(plotlist = p, common.legend = TRUE, nrow = 1, ncol = 2))
 dev.off()
