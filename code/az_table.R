48cdff289883c018b4ad5717806859050b6212de
ArvinZaker <arv.zaker@gmail.com>
2024-03-08 22:20:33 -0500

    added the table making func

---
+++ code/az_table.R
@@ -0,0 +1,137 @@
+# NOTE: this figure was modified from the old script to include the new
+# results (REF, MRMR, and GA)
+library(readxl)
+library(ggplot2)
+library(ggpubr)
+library(writexl)
+library(tidyr)
+library(grid)
+library(gridExtra)
+library(kableExtra)
+library(xtable)
+library(pander)
+count_decimals <- function(x) {
+  # length zero input
+  if (length(x) == 0) {
+    return(numeric())
+  }
+
+  # count decimals
+  x_nchr <- x %>%
+    abs() %>%
+    as.character() %>%
+    nchar() %>%
+    as.numeric()
+  x_int <- floor(x) %>%
+    abs() %>%
+    nchar()
+  x_nchr <- x_nchr - 1 - x_int
+  x_nchr[x_nchr < 0] <- 0
+
+  x_nchr
+}
+
+formatXLSX <- function(x, valCol = "pearsonCor") {
+  inMat <- x[, c("drug", "feature selection method", valCol)] # "feature selection method"
+  colnames(inMat) <- c("drug", "Type", "value")
+  inMat <- as.data.frame(inMat)
+  wideMat <- tidyr::pivot_wider(inMat, names_from = "Type", values_from = "value")
+  wideMat <- as.data.frame(wideMat)
+  rownames(wideMat) <- wideMat$drug
+  wideMat$drug <- NULL
+  means <- c()
+  sds <- c()
+  for (i in colnames(wideMat)) {
+    x <- as.vector(unlist(wideMat[, i]))
+    means[i] <- mean(x)
+    sds[i] <- sd(x)
+  }
+  z <- abs(qnorm(0.05))
+  n <- nrow(wideMat)
+  sme <- sds / sqrt(n)
+
+
+  # means <- signif(means, 4)
+  means <- signif(means, 4)
+  dec <- min(count_decimals(means))
+  ciMat <- data.frame(
+    drug = colnames(wideMat),
+    mean = (round(means, dec)),
+    lowCI = (round(means - (z * sds / n), dec)),
+    highCI = (round(means + (z * sds / n), dec)),
+    sme = round(sme, dec)
+  )
+  return(ciMat)
+}
+h <- 4
+
+methods <- c("pearsonCor", "spearmanCor", "kendallCor", "RMSE", "MSE", "MAE") #
+# "pearsonCor",
+
+generateTable <- function(inFile, inSheet) {
+  space <- "  "
+  tableMat <- data.frame()
+  rowOrder <- c(
+    "var-100", "var-500", "L1000", "L1000-tm", "cor-500",
+    "RFE", "MRMR", "GA",
+    "text-mining"
+  )
+  for (i in methods) {
+    print(i)
+    input <- read_xlsx(inFile, sheet = inSheet)
+    input <- formatXLSX(input, valCol = i)
+    ref <- input["text-mining", "mean"]
+    input$sig <- (input$highCI <= ref | input$lowCI >= ref)
+    input$sig <- ifelse(input$sig == TRUE, "*", "")
+    input$text <- paste0(space, input$mean, " (±", input$sme, ")", input$sig, space)
+    tableMat[input$drug, i] <- input$tex # t
+  }
+  ylabs <-
+    c(
+      "pearsonCor" = "Pearson",
+      "spearmanCor" = "Spearman",
+      "kendallCor" = "Kendall",
+      "RMSE" = "RMSE",
+      "MSE" = "MSE",
+      "MAE" = "MAE"
+    )
+  colnames(tableMat) <- ylabs[colnames(tableMat)]
+  row_colors <- rep(c("white", "grey90"), length.out = nrow(tableMat))
+  coln <- colnames(tableMat)
+  newColn <- "Feature selection\nmethod"
+  tableMat[, newColn] <- rownames(tableMat)
+  tableMat <- tableMat[, c(newColn, coln)]
+
+  tableMat <- tableMat[rev(rowOrder), ]
+  tableMat <- na.omit(tableMat)
+
+  grid.table(tableMat,
+    rows = NULL,
+    theme = ttheme_default(
+      core = list(
+        bg_params = list(fill = row_colors),
+        size = 2
+      ),
+      rowhead = list(
+        # bg_params = list(fontface = "plain") # Set row names font to plain
+      ),
+      hline = list(col = "grey", lwd = 0.5), # Horizontal lines
+      vline = list(col = "grey", lwd = 0.5), # Vertical lines
+      # padding = unit(c(4, 2), "mm")
+    )
+  )
+}
+pdf("./table.pdf", height = 3.5, width = 12)
+generateTable(inFile = "./mlModelMetrics.xlsx", inSheet = "RandomForest perf metrics-test")
+grid.newpage()
+generateTable(inFile = "./mlModelMetrics.xlsx", inSheet = "ElasticNet perf metrics-test")
+grid.newpage()
+generateTable(
+  inFile = "./result/Supplementary-data-2-performance-indexes.xlsx",
+  inSheet = "DeepLearning perf metrics"
+)
+
+
+dev.off()
+
+print("done")
